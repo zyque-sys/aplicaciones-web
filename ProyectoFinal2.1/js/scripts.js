b4a8bd60f247f3c0efa4afcc4166e4079b47fe78
Nayla Leyva <180416@upbc.edu.mx>
2021-04-17 08:00:25 -0700

    Proyecto juego de memoria

--- ProyectoFinal2.1/js/scripts.js
+++ ProyectoFinal2.1/js/scripts.js
@@ -1,106 +1,34 @@
-class JuegoMemoria {
-    constructor(cards) {
-        this.cardsArray = cards;
-        this.ticker = document.getElementById('movesamt');
-    }
+const cards = document.querySelectorAll('.square');
 
-    startGame() {
-        this.totalClicks = 0;
-        this.cardToCheck = null;
-        this.matchedCards = [];
-        this.busy = true;
-        setTimeout(() => {
-            this.shuffleCards(this.cardsArray);
-            this.busy = false;
-        }, 500)
-        this.hideCards();
-        this.ticker.innerText = this.totalClicks;
-    }
-    
-    victory() {
-        document.getElementById('victory-text').classList.add('visible');
-    }
-    hideCards() {
-        this.cardsArray.forEach(card => {
-            card.classList.remove('visible');
-            card.classList.remove('matched');
-        });
-    }
+let hasFlippedCard = false;
+let card1, card2;
+function init() {
+    hasFlippedCard=false;
+    cards.forEach(cards => cards.addEventListener('click', flipCard));
+}
+cards.forEach(cards => cards.addEventListener('click', flipCard));
 
-    checkForCardMatch(card) {
-        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
-            this.cardMatch(card, this.cardToCheck);
-        else 
-            this.cardMismatch(card, this.cardToCheck);
+function flipCard() {
+  this.classList.add('flip');
 
-        this.cardToCheck = null;
-    }
-    flipCard(card) {
-        if(this.canFlipCard(card)) {
-            this.totalClicks++;
-            this.ticker.innerText = this.totalClicks;
-            card.classList.add('visible');
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    card1 = this;
+  } else {
+    hasFlippedCard = false;
+    card2 = this;
 
-            if(this.cardToCheck) {
-                this.checkForCardMatch(card);
-            } else {
-                this.cardToCheck = card;
-            }
-        }
+    if (card1.datakey === card2.datakey) {
+      
+      card1.removeEventListener('click', flipCard);
+      card2.removeEventListener('click', flipCard);
+    } else {
+      setTimeout(() => {
+        card1.classList.remove('flip');
+        card2.classList.remove('flip');
+      }, 1200);
     }
-    cardMatch(card1, card2) {
-        this.matchedCards.push(card1);
-        this.matchedCards.push(card2);
-        card1.classList.add('matched');
-        card2.classList.add('matched');
-        this.audioController.match();
-        if(this.matchedCards.length === this.cardsArray.length)
-            this.victory();
-    }
-    cardMismatch(card1, card2) {
-        this.busy = true;
-        setTimeout(() => {
-            card1.classList.remove('visible');
-            card2.classList.remove('visible');
-            this.busy = false;
-        }, 1000);
-    }
-    shuffleCards(cardsArray) { 
-        for (let i = cardsArray.length - 1; i > 0; i--) {
-            let randIndex = Math.floor(Math.random() * (i + 1));
-            cardsArray[randIndex].style.order = i;
-            cardsArray[i].style.order = randIndex;
-        }
-    }
-    getCardType(card) {
-        return card.getElementsByClassName('card-value')[0].src;
-    }
-    canFlipCard(card) {
-        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
-    }
+  }
 }
 
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready);
-} else {
-    ready();
-}
 
-function ready() {
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new JuegoMemoria(cards);
-
-    overlays.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('visible');
-            game.startGame();
-        });
-    });
-
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            game.flipCard(card);
-        });
-    });
-}
