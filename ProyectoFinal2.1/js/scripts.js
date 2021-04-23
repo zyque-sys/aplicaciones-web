cb045bddce02fa7425c55b3d0e023dc830bd2ccf
Nayla Leyva <180416@upbc.edu.mx>
2021-04-23 11:38:38 -0700

    proyectous

--- ProyectoFinal2.1/js/scripts.js
+++ ProyectoFinal2.1/js/scripts.js
@@ -1,53 +1,125 @@
-const cards = document.querySelectorAll('.square');
 
+let cards = document.querySelectorAll('.square');
+let cardsamt;
+let movesamt;
+let cardSets=[];
+let cardCouples=[];
+const url= "https://rickandmortyapi.com/api/character/";
+var flips = document.querySelector('.flips');
 let hasFlippedCard = false;
-let card1, card2;
+let card1, card2=[];
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const modal = document.getElementById("modal-result");
+  const exit = document.querySelector('.close');
+
+  exit.onclick = function() {
+      
+      modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+      if(event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+});
+init();
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 function init() {
-    hasFlippedCard=false;
-    cards.forEach(cards => cards.addEventListener('click', flipCard));
+    setCards();
    
 }
+function setMoves() {
+  flips.innerHTML = `Movimientos: ${movesamt}`;
+}
+async function setCards() {
+  hasFlippedCard = true;
 
-const render = (data, dataRicknMorty) => {
-	let html = data
-		.map((elem) => {
-			var randomnumber = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
-			var image = dataRicknMorty.results[randomnumber].image;
-			return `
-            <div class="photo">
-                <img src="${image}" alt="${dataRicknMorty.results[randomnumber].name}" height="50px" />
-            </div>
-        `;
-		})
-		.join(' ');
+  cardsamt = 0;
+  movesamt = 0;
+  cardSets = [];
+  cardCouples = [];
+  card1 = [];
+  card12 = [];
+  const response = await fetch(`${url}`);
+  setMoves();
 
-	document.getElementById('square').innerHTML = html;
-};
-
-
-
-function flipCard() {
-  this.classList.add('flip');
-
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    card1 = this;
-  } else {
-    hasFlippedCard = false;
-    card2 = this;
-
-    if (card1.datakey === card2.datakey) {
-      
-      card1.removeEventListener('click', flipCard);
-      card2.removeEventListener('click', flipCard);
-    } else {
-      setTimeout(() => {
-        card1.classList.remove('flip');
-        card2.classList.remove('flip');
-      }, 1200);
-    }
+  for (let i = 0; i < 16; i++) {
+      cardsamt++;
+      cardSets[i] = "https://rickandmortyapi.com/api/character/avatar/" + cardsamt + ".jpeg";
+      if (cardsamt == 8) {
+          cardsamt = 0;
+      }
   }
+  cardSets.sort(() => Math.random() - 0.5);
+}
+function restart() {
+  location.reload();
+}
+async function isCouple(){
+  movesamt++;
+  setMoves();
+  if(card2[0] == card2[1]){
+      cardCouples.push(card1.pop());
+      cardCouples.push(card1.pop());
+      hasFlippedCard= true;
+      return true;
+  }
+  await sleep(900);
+  card1.pop().innerHTML = ``;
+  card1.pop().innerHTML = ``;
+  hasFlippedCard = true;
+  return false;
 }
 
+async function flipCards(number) {
+  if (!hasFlippedCard) return;
+  if (GameOver()) return;
+  
+  for (let i = 0; i < cardCouples.length; i++) {
+      const element = cardCouples[i];
+      if (element.dataset.key == cards[number].dataset.key) {
+          return;
+      }
+  }
+
+  if (card1.length == 0) {
+      
+      card1.push(cards[number]);
+      card1[0].innerHTML = `<img src="${cardSets[number]}" >`
+      card2[0] = cardSets[number];
+      return;
+  }
+
+  if (card1[0].dataset.key === cards[number].dataset.key)  return;
+ 
+  hasFlippedCard = false;
+  card1.push(cards[number]);
+  card1[1].innerHTML = `<img src="${cardSets[number]}" >`
+  card2[1] = cardSets[number];
+  doMatch();
+  if (GameOver()) return;
+}
+
+
+
+function GameOver() {
+
+  if (CompletedCards.length == 16) {
+      findWinner();
+      return true
+  }
+  return false;
+}
+function findWinner() {
+   const modal = document.getElementById('modal-result');
+   modal.style.display = "block";
+
+   const winnerTextResult = document.querySelector('#winner-result-text');
+   winnerTextResult.innerHTML = `Movimientos: ${movesamt}`;
+}
 
---
+++ ProyectoFinal2.2/js/script.js
@@ -0,0 +1,143 @@
+let dice1 = document.getElementById("dice1");
+let dice2 = document.getElementById("dice2");
+let turnP1 = document.getElementById("player1");
+let turnP2 = document.getElementById("player2");
+var scoreP1 = document.getElementById("totalScoreP1");
+var scoreP2 = document.getElementById("totalScoreP2");
+var tempscoreP1 = document.getElementById("tempScoreP1");
+var tempscoreP2 = document.getElementById("tempScoreP2");
+var turn = 1;
+var dice = "img/dices.png";
+var Dices = ["img/dice1.png", "img/dice2.png", "img/dice3.png", "img/dice4.png", "img/dice5.png", "img/dice6.png"];
+var num1;
+var num2;
+var sum;
+
+
+var winner;
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const modal = document.getElementById("modal-result");
+    const exit = document.querySelector('.close');
+
+    exit.onclick = function () {
+
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
+init();
+async function init() {
+    sum = 0;
+    scoreP1 = 0;
+    scoreP2 = 0;
+    tempScoreP1 = 0;
+    tempScoreP2 = 0;
+    winner = "";
+    turn = 1;
+    dice1.innerHTML = `<img src="${dice}" >`;
+    dice2.innerHTML = `<img src="${dice}" >`;
+    turnP1.classList.remove("active");
+    turnP2.classList.remove("active");
+    turnP1.classList.add("active");
+    tempscoreP1.innerHTML = `${tempScoreP1}`;
+    scoreP1.innerHTML = `${scoreP1}`;
+    tempscoreP2.innerHTML = `${tempScoreP2}`;
+    scoreP2.innerHTML = `${scoreP2}`;
+}
+
+
+
+async function RollDice() {
+    
+    var num1 = Math.floor(Math.random() * 6) + 1;
+    var num2 = Math.floor(Math.random() * 6) + 1;
+    dice1.innerHTML = `<img src="dice${dices[num1 - 1]}.png" >`
+    dice2.innerHTML = `<img src="dice${dices[num2 - 1]}.png" >`
+    Turns(num1, num2);
+}
+
+function Shift() {
+    if (turn == 1) {
+        scoreP1 += tempScoreP1;
+        tempScoreP1=0;
+        turn = 2;
+        turnP1.classList.remove("active");
+        turnP2.classList.add("active");
+        tempscoreP1.innerHTML = `${tempScoreP1}`;
+        scoreP1.innerHTML = `${scoreP1}`;
+        if(GameOver()) return;
+        return turn;
+    }
+    if (turn == 2) {
+        scoreP2 += tempScoreP2;
+        tempScoreP2=0;
+        turn = 1;
+        turnP1.classList.add("active");
+        turnP2.classList.remove("active");
+        tempscoreP2.innerHTML = `${tempScoreP2}`;
+        scoreP2.innerHTML = `${scoreP2}`;
+        if(GameOver()) return;
+
+        return turn;
+    }
+}
+
+async function Turns(num1, num2) {
+    if (num1 == num2) {
+        tempScoreP1 = 0;
+        tempScoreP2 = 0;
+        Shift();
+        return
+    }
+    if (turn == 1) {
+        tempScoreP1 += num1 + num2;
+        tempscoreP1.innerHTML = `${tempScoreP1}`;
+    }
+    if (turn == 2) {
+        tempScoreP2 += num1 + num2;
+        tempscoreP2.innerHTML = `${tempScoreP2}`;
+    }
+
+}
+
+
+
+function GameOver() {
+    if (scoreP1 >= 100) {
+        winner = "Player1"
+        showModal();
+        return true;
+    }
+    if (scoreP2 >= 100) {
+        winner = "Player2"
+        showModal();
+        return true;
+    }
+    return false
+}
+
+function showModal() {
+
+    // Mostrar ganador.
+    console.log('Alguien ganó');
+
+    const modal = document.getElementById('modal-result');
+    modal.style.display = "block";
+
+    const winnerTextResult = document.querySelector('#winner-text-result');
+    winnerTextResult.innerHTML = `Winner: ${winner}`;
+}
+function restart() {
+    location.reload();
+}
